0af33a97dd286a30bd28316c77073e7e47a3a7b9
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-01-19 16:33:41 -0300

    Desenvolvimento head

--- recipes/views.py
+++ recipes/views.py
@@ -3,5 +3,7 @@ from django.shortcuts import render
 
 # HTTP request
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Ernesto',
+    })
     # return HTTP response
