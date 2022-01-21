53446928677a127122f3315608a341890ae60e7d
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-01-21 17:39:34 -0300

    Recipes/Utils

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 
 # HTTP request
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'Ernesto',
+        'recipes': [make_recipe() for _ in range(10)],
     })
     # return HTTP response
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
