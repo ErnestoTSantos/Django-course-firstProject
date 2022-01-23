39afea38bc4c3f79fcf89688b3a0008cf1792187
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-01-23 19:11:55 -0300

    Ajuste posição

--- FirstAnimationProject/urls.py
+++ FirstAnimationProject/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import \
-    settings  # Forma de importar o settings do projeto da melhor maneira
+# Forma de importar o settings do projeto da melhor maneira
+from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
