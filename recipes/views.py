9bdd0c4d40fcfd9b03108042d80ffbd4d48af519
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-01-23 19:12:58 -0300

    Ajuste de 404

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
-from django.http import Http404
+# from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from . import models
 
 
 def home(request):
+    recipe = get_list_or_404(models.Recipe.objects.all().order_by('?'))
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': models.Recipe.objects.all().order_by('?'),
+        'recipes': recipe,
     })
 
 
@@ -20,7 +22,8 @@ def category(request, category_id):
 
     recipes = get_list_or_404(models.Recipe.objects.filter(
         # Mandamos a query inteira, para podermos usar o order_by
-        category__id=category_id).order_by('?')
+        category__id=category_id,
+        is_publisher=True).order_by('?')
     )
 
     return render(request, 'recipes/pages/category.html', context={
@@ -30,7 +33,10 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = models.Recipe.objects.all().filter(id=id).first()
+    # Outra forma para pegar uma recita
+    # recipe = models.Recipe.objects.all().filter(id=id).first()
+
+    recipe = get_object_or_404(models.Recipe, id=id, is_publisher=True)
 
     return render(request, 'recipes/pages/recipe-view.html', context={
         'recipe': recipe,
