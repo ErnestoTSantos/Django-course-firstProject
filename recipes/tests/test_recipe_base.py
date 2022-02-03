0c612761d46caea12fa4192638a6c1045c2a4b6a
ErnestoTSantos <ernesto.terra2003@gmail.com>
2022-02-03 17:42:08 -0300

    Comentário sobre testes

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -2,6 +2,8 @@ from django.contrib.auth.models import User
 from django.test import TestCase
 from recipes.models import Category, Recipe
 
+# Quanto mais campos mais testes para realizar as execuções da melhor maneira
+
 
 class RecipeTestBase(TestCase):
 
